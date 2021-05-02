eeb54bbf126c4ef54706f0be4df8366de405721e
c0natus <hks8946@gmail.com>
2021-05-03 03:01:56 +0900

    vt.json

--- scanFile/scanFile.py
+++ scanFile/scanFile.py
@@ -1,4 +1,73 @@
-import requests, json
+import requests, os
 
-def virustotal_upload(orgfile):
-    url = '
+API_KEY = "45e688ec223ad38565518fd51c7455a1a9f219638959a7053b53eb8fbb7e623a"
+
+
+def file_upload(orgfile, timeout=None, proxies = None):
+    if not os.path.isfile(orgfile):
+        raise Exception('File not found.')
+    base_url = 'https://www.virustotal.com/api/v3/files'
+    file_size = os.path.getsize(orgfile)
+    headers = {
+        'x-apikey': API_KEY,
+    }
+    # 32 MB 기준
+    if file_size >= 33554432:
+        with open(orgfile, 'rb') as f:
+            data = {'file': f.read()}
+            try:
+                response = requests.get(base_url + '/upload_url',
+                                        headers=headers,
+                                        proxies=proxies,
+                                        timeout=timeout)
+
+                if response.status_code != 200:
+                    raise Exception(response)
+
+                upload_url = response.json()['data']
+                response = requests.post(upload_url,
+                                         headers=headers,
+                                         files=data,
+                                         proxies=proxies,
+                                         timeout=timeout)
+                if response.status_code != 200:
+                    raise Exception(response)
+
+                return response.json()
+
+            except Exception as e:
+                print(e)
+                exit(1)
+    else:
+        with open(orgfile, 'rb') as f:
+            data = {'file': f.read()}
+            try:
+                response = requests.post(base_url,
+                                         headers=headers,
+                                         files=data,
+                                         proxies=proxies,
+                                         timeout=timeout)
+
+                if response.status_code != 200:
+                    raise Exception(response)
+
+                return response.json()
+
+            except Exception as e:
+                print(e)
+                exit(1)
+
+
+def file_report(file_id):
+    headers = {
+        'x-apikey': API_KEY,
+        'Accept': 'application/json',
+    }
+    response = requests.get('https://www.virustotal.com/api/v3/analyses/{}'.format(file_id), headers=headers)
+    return response.json()
+
+
+if __name__ == '__main__':
+    orgfile = 'test.hwp'
+    file_id = file_upload(orgfile)['data']['id']
+    report_json = file_report(file_id)
