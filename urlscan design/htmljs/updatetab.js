6e90b8412411faa586a3f223ef19d4350a240923
JiHoon97 <58676668+Userz1-redd@users.noreply.github.com>
2021-05-18 16:39:46 +0900

    Delete urlscan design directory

--- urlscan design/htmljs/updatetab.js
+++
@@ -1,8 +0,0 @@
-var receiveurl;
-document.addEventListener('DOMContentLoaded',function(){
-	var button1 = document.getElementById("update");
-	button1.addEventListener("click",function(){
-		chrome.runtime.sendMessage({action: "FINISH"}, function(response) {
-		});
-});
-});
